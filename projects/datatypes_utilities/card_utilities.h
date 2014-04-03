3e5f4a973fb4adc9a6d3f7f8d83dddae502570ce
Wesley Ketchum <wketchum@lanl.gov>
2014-04-03 11:41:47 -0500

    add in new utilities folder, and start with a function to calculate the checksum

---
+++ projects/datatypes_utilities/card_utilities.h
@@ -0,0 +1,34 @@
+#ifndef _UBOONETYPES_CARDUTILITIES_H
+#define _UBOONETYPES_CARDUTILITIES_H
+
+/***
+    This is for common utilities to aid in looking at serealized data
+    (or perhaps raw binary data too...?). Things that are meant to modify
+    data products should not go here. It's also likely things that are meant to
+    unpack or decode data products need not go here.
+ ***/
+
+#include "datatypes/cardHeader.h"
+#include "datatypes/cardData.h"
+#include "datatypes/channelData.h"
+
+namespace gov {
+namespace fnal {
+namespace uboone {
+namespace datatypes {
+namespace utilities {
+
+  using namespace gov::fnal::uboone::datatypes;
+
+  //gives difference between header and sum of card data
+  long compareCheckSum(cardHeader const& cH, cardData const& cD);
+  uint32_t getCheckSumFromDataBlock(const char* data_ptr,size_t data_size);
+  uint32_t getCheckSumFromChannel(channelData const& chD);
+
+}
+}
+}
+}
+}
+
+#endif
