40c870888d6fa305ca202f8f6ddde4fecfdf1be4
georgiak <georgiak@fnal.gov>
2015-05-06 16:01:07 -0500

    Updated ups instalation function

--- projects/tools/helper_functions.sh
+++ projects/tools/helper_functions.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
 # Setup UPS
-source /uboone/setup
-
 
 function setup_privateups_repository
 {
 if [ -z "${PRIVATE_UPS_DIR}" ]; then
 echo Error: PRIVATE_UPS_DIR is not set. 
-echo Set it with \"export PRIVATE_UPS_DIR=${HOME}/install/privateups\" and rerun this command.
+echo Set it with \"export PRIVATE_UPS_DIR=${HOME}/install\" and rerun this command.
+
 
 else
 echo Setting up a private UPS repository in ${PRIVATE_UPS_DIR}
 
 THIS_DIR=`pwd`
-source /uboone/setup
 
-setup ups
 #configure a private ups repository for development
 #needs to be done once
-rm -rf $PRIVATE_UPS_DIR; mkdir -p $PRIVATE_UPS_DIR/ups; mkdir -p $PRIVATE_UPS_DIR/upd ; setup upd
-TMP_STR=`which ups` && cp -ra ${TMP_STR%/Linux*}*  $PRIVATE_UPS_DIR/ups/
-TMP_STR=`which ups` && cp -ra ${TMP_STR%/Linux*}/../current.chain  $PRIVATE_UPS_DIR/ups/
-TMP_STR=`which ups` && cp -ra ${TMP_STR%/Linux*}/../../setup  $PRIVATE_UPS_DIR/
-TMP_STR=`which ups` && cp -ra ${TMP_STR%/Linux*}/../../.upsfiles  $PRIVATE_UPS_DIR/
-TMP_STR=`which upd` && cp -ra ${TMP_STR%/NULL*}*  $PRIVATE_UPS_DIR/upd/
-#TMP_STR=`which upd` && cp -ra ${TMP_STR%/NULL*}/../current.chain  $PRIVATE_UPS_DIR/upd/
-TMP_STR=`find $PRIVATE_UPS_DIR/ups/ -name buildUps.sh` && cd ${TMP_STR%/buildUps.sh*} && $TMP_STR
+rm -rf $PRIVATE_UPS_DIR; mkdir -p  $PRIVATE_UPS_DIR; cd $PRIVATE_UPS_DIR;
+ls -tr `find /uboone*/dist* -name ups-*Linux*.tar.bz2` |tail -1 |xargs -n 1 -I {} tar jxf {}
 source ${PRIVATE_UPS_DIR}/setup
-
 cd $THIS_DIR
 fi
 }
