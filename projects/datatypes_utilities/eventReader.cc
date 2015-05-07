bd1fa7aa97c4a88f245e55131b2b544241feedf9
Wesley Ketchum <wketchum@lanl.gov>
2015-05-07 11:04:55 -0500

    adding in more samples for data access

--- projects/datatypes_utilities/eventReader.cc
+++ projects/datatypes_utilities/eventReader.cc
@@ -31,12 +31,76 @@ int main(int argc, char **argv)
   std::ifstream is ( fileName , std::ios::binary | std::ios::in);
   int i{0};
   try{while(true){
+
+      
+    /*
+      Boost Readin:
+      This is how you read in an event from a file. Note, you MUST make a 
+      new binary_iarchive per for every event. The read command advances
+      the file stream.
+
+      The basic data unit is the EventRecord. Everything about the event should
+      be contained within it.
+    */
     boost::archive::binary_iarchive ia ( is );      
     ub_EventRecord  eventRecord;
     ia >> eventRecord;
     std::cout << "+++++ Event: " << ++i << "\n";
-    std::cout << eventRecord.debugInfo();        
-  
+
+    /*
+      Debug info:
+      You can use this as a utility to print what's in
+      the event. I've commented it out for this sample file.
+    */
+    //std::cout << eventRecord.debugInfo();        
+
+    /*
+      Global header:
+      Events should contain a global header. To access that, you do the following.
+      Note: the global_header_t typedef is defined in raw_data_access.h
+      See ub_GlobalHeader.h for the members.
+    */
+    global_header_t const& globalHeader = eventRecord.getGlobalHeader();
+    std::cout << globalHeader.debugInfo();
+
+    /*
+      Accessing SEB fragments:
+      Events contain maps of different fragment types (TPC or PMT for the moment).
+      You can get the total number of fragments...
+    */
+    size_t n_total_fragments = eventRecord.getFragmentCount();
+
+    /*
+      TPC fragment map:
+      The fragments themselves are contained in separate map classes.
+      The tpc seb map is an <int,tpc_crate_data_tuple_t> pair. That tuple contains
+      ---<0> a raw_data_fragment_t, which is a vector of uint16_t. This is the data!
+      ---<1> a ptr to a ub_RawData object, which is the pointer to be used for accessing
+             that data.
+      ---<2> a ptr to a tpc_crate_data_t, which is a typedef for the TPC Crate data object, 
+             which can be used for accessing that data in an interpreted way (something that
+	     understands the headers, data format, etc.)
+      In most cases, you want access through the last one, as that contains the info needed
+      to get to the information directly.
+    */
+
+    //This is a range based for loop, where we loop over the elements of the map.
+    for(auto const& tpc : eventRecord.getTPCSEBMap()){
+
+      int tpc_crate_num = tpc.first;
+      tpc_crate_data_t const& tpc_crate = tpc.second;
+
+      //The tpc crate data object contains access to a header, trailer, and the internal data.
+      //You have to follow the typedefs, but the header and trailer for the TPC crate data are
+      //ub_XMITEventHeader and ub_XMITEventTrailer objects. You find the header files for those,
+      //and you can find that they are uint32_t types. I'd recommend using auto here to access:
+      auto const& tpc_crate_header = tpc_crate.header();
+      auto const& tpc_crate_trailer = tpc_crate.trailer();
+      
+    }
+    
+
+    
   }}catch(...){
   std::cout << "Done.";
   }
