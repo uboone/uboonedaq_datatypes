a8634e4ce2863ca5d06fa8412b176fa5236d4cc1
Nathaniel Tagg <tagg@fnal.gov>
2015-05-13 11:14:32 -0500

    Refactor huffman into header file due to link errors

--- projects/datatypes/ub_TPC_ChannelData_v6.h
+++ projects/datatypes/ub_TPC_ChannelData_v6.h
@@ -22,6 +22,82 @@ public:
 };
 
 
+template<typename T>
+void ub_TPC_ChannelData_v6::decompress(std::vector<T>& uncompressed) const throw(datatypes_exception)
+{
+  /// 
+  /// Huffman decompression.
+  ///
+  /// Fills the supplied vector with huffman-decompressed data.
+  /// Note that the user should check the vector to make sure that the size is what is expected (e.g. size()==9600)
+  /// Won't throw if output size is wrong
+  /// WILL throw if there's an unrecognized bit pattern.
+  ///
+  /// Available as T= uint_16, int16, uint_32, int32, float, and double. 
+  /// Why make so many types available? Because it's a needless extra expense in both memory and 
+  /// cpu time to make a copy.  Online monitor, for instance, likes to use doubles because they translate well
+  /// into the TH1::Fill commands.  
+  ///
+  /// 
+  /// N. Tagg. May 2015
+  ///
+
+  // This saves a fair bit of time in the long run.
+  // It's much faster to resize() and use operator[] than to reserve, but this is the safest in case of buffer overruns.
+  //
+  const size_t kMaxBufferSize = 9600;
+  
+  uncompressed.resize(0); // This doesn't free memory if the vector is already allocated. 
+  uncompressed.reserve(kMaxBufferSize);  // Enlarge if buffer is too small. No effect if buffer is already large.
+
+  const ub_RawData& raw = data();
+  ub_RawData::const_iterator it;
+  uint16_t last_uncompressed_word = 0;
+
+  
+  // Dump check for debugging.
+  for(it = raw.begin(); it!= raw.end(); it++) {
+
+    uint16_t word = *it;
+    //if it's not a compressed word, just put it on the uncompressed vector
+    if( (word & 0x8000)==0 ) {
+      last_uncompressed_word = word&0x7ff;
+      uncompressed.push_back(  (T)(last_uncompressed_word)  ); // explicit conversion to templated type
+    
+    } else {  // huffman bit on.
+      uint16_t outword;
+      size_t zero_count = 0;
+      bool   non_zero_found = false;
+      for(size_t index=0; index<16; ++index){
+        if( !((word >> index) & 0x1) ) {
+          if(non_zero_found) zero_count ++; // Count zeros IF we're past the padding in the right-hand bits.
+        }else{
+          if(!non_zero_found) non_zero_found= true;
+          else {
+            switch(zero_count) { // subst 
+              case 0: outword = last_uncompressed_word;    break;
+              case 1: outword = last_uncompressed_word -1; break;
+              case 2: outword = last_uncompressed_word +1; break;
+              case 3: outword = last_uncompressed_word -2; break;
+              case 4: outword = last_uncompressed_word +2; break;
+              case 5: outword = last_uncompressed_word -3; break;
+              case 6: outword = last_uncompressed_word +3; break;
+              default:
+              // std::cout << "Huffman decompress unrecoginized bit pattern " << (std::bitset<16>) word << std::endl;
+                throw datatypes_exception("Huffman decompress unrecoginized bit pattern");                
+            }
+            uncompressed.push_back( (T)(outword) );
+            last_uncompressed_word = outword;   // Activite this line is delta is from last word. Comment out this line if diff is from the last EXPLICIT word, instead of the last huffman-compressed word.
+            zero_count=0;
+          }
+        }
+      }
+      
+    } //end else huffman bit on
+  }//end for loop over data words
+
+} 
+
 }  // end of namespace datatypes
 }  // end of namespace uboone
 }  // end of namespace fnal
