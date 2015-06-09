2a45b98b04ee6c024bb81109eca731d94085f13c
echurch <echurch@fnal.gov>
2015-06-09 00:38:27 -0500

    dumpEventHeaders.cc can now ffwd to desired event quickly. Also gives back 0 (\!0) status on (un)successful return.

--- projects/datatypes_utilities/dumpEventHeaders.cc
+++ projects/datatypes_utilities/dumpEventHeaders.cc
@@ -22,6 +22,8 @@ int main(int argc, char **argv)
   std::string fileName;
   int eventNumber(-12);
   int nevents(0);
+
+  // Some file-record-hopping code stolen/repurposed from online_monitoring/DaqFile.
   
   switch(argc-1)
   {
@@ -39,13 +41,25 @@ int main(int argc, char **argv)
   std::ifstream is ( fileName , std::ios::binary | std::ios::in);
   int i{0};
   bool closedCleanly (false);
+  std::vector<uint32_t>  index_buffer;  // sizes of each event
+  int status = 1;
 
   try{
     is.seekg(-6,is.end); // Go to position 6 bytes before end of file. 
     uint8_t buff[10];
     is.read((char*) buff, 6);
     nevents = *(uint32_t*)(buff);
-    if (nevents>0 && nevents <1E9) closedCleanly = true; 
+    uint16_t endOfFileMarker = *(uint16_t*)(buff+4);
+    if(endOfFileMarker == 0xe0f0 && (nevents>0 && nevents <1E9) )
+      closedCleanly = true; 
+   
+    uint64_t index_pos = 6 + sizeof(uint32_t)*nevents;
+    is.clear();
+    index_buffer.resize(nevents);
+    is.seekg(-index_pos,is.end);
+    is.read((char*)(&index_buffer[0]),sizeof(uint32_t)*nevents);   
+    is.seekg(0,is.beg);
+   
   }
   catch(...)
   {
@@ -59,22 +73,47 @@ int main(int argc, char **argv)
     std::cout << " There are " << std::dec << nevents << " events in this file." << std::endl;
     if (eventNumber > nevents) eventNumber = nevents;
 
-    while(true){
-      boost::archive::binary_iarchive ia ( is );      
-      ub_EventRecord  eventRecord;
-      ia >> eventRecord;
-      ++i;
-      if (i != eventNumber) continue;
-      std::cout << "+++++ Event: " << i << "\n";
+    if (!closedCleanly) // walk evt-by-evt
+      {
+	while(true){
+	  boost::archive::binary_iarchive ia ( is );      
+	  ub_EventRecord  eventRecord;
+	  ia >> eventRecord;
+	  ++i;
+	  if (i != eventNumber) continue;
+	  std::cout << "eventRecord.size is " << sizeof(eventRecord) << std::endl;
+	  global_header_t const& globalHeader = eventRecord.getGlobalHeader();
+	  std::cout << globalHeader.debugInfo() << std::endl;
+	  break;
+	}
+	std::cout << "+++++ Event: " << i << "\n";
+	status = 0;
+      }
+
+    else // ffwd to desired event
+      {
+	boost::archive::binary_iarchive ia ( is );      
+	ub_EventRecord  eventRecord;
+	size_t offset (0);
+	for(int j=0;j<(eventNumber-1);j++) 
+	  { offset += index_buffer[j];
+	  }
+	is.seekg(offset,is.cur);
+
+	std::cout << " Offset is " << offset << std::endl;
+	ia >> eventRecord;
+	std::cout << "sizeof(eventRecord) is " << sizeof (eventRecord) << std::endl;
+	global_header_t const& globalHeader = eventRecord.getGlobalHeader();
+	std::cout << globalHeader.debugInfo() << std::endl;
+	status = 0;
+	
+      }
 
 
-      global_header_t const& globalHeader = eventRecord.getGlobalHeader();
-      std::cout << globalHeader.debugInfo() << std::endl;
-      break;
-      
-    }
   }catch(...){
     std::cout << "Done. closedCleanly?: " << closedCleanly << std::endl;
-
+    status = 2;
   }
+
+  return status;
 }
