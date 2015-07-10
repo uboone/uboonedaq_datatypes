458549019d2ab4993892f930e6262c0e34b33e16
Gennadiy Lukhanin <lukhanin@ubdaq-prod-evb.fnal.gov>
2015-07-10 14:08:44 -0500

    Disabled PMT checksum validation

--- projects/datatypes/ub_PMT_CardData_v6.cpp
+++ projects/datatypes/ub_PMT_CardData_v6.cpp
@@ -12,7 +12,7 @@ bool ub_MarkedRawCardData<ub_PMT_ChannelData_v6,ub_PMT_CardHeader_v6,ub_PMT_Card
 {
     if(!verify_checksum( data(),  header().getChecksum())){
       std::cerr << "Wrong channel data checksum\n";
-      return false;
+      return true; //continue for now
     }    
    return true;
 }
