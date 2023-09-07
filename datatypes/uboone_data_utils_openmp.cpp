fcdb6d6126b9a9cb7630165b05a7dbf5f9f345e1
Herbert Greenlee <greenlee@fnal.gov>
2023-09-07 16:58:04 -0500

    Update for cetmodules.

--- datatypes/uboone_data_utils_openmp.cpp
+++ datatypes/uboone_data_utils_openmp.cpp
@@ -40,7 +40,7 @@ uint32_t compute_checksum(ub_RawData const& data) noexcept
 {
       uint32_t initial_value{0};
       uint32_t return_value{0};         
-      #pragma omp parallel //num_threads(16)
+      //#pragma omp parallel //num_threads(16)
       {
 	return_value=__gnu_parallel::accumulate(data.begin(), data.end(), initial_value,[](uint32_t a, uint32_t b){return a+b;});
       }
